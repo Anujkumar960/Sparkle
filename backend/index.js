1fcddcc9bd350cf032ddd458b0785dd854710684
Anuj <anujkshatriya82@gmail.com>
2024-02-25 17:08:25 +0530

    Cors added

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const cor= require('cors');
+const cors= require('cors');
 
 server.db = router.db;
 
